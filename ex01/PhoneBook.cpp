effeac1a6d851e75aad42ebf2f56e41c05cf475a
Ayoub El Mouaze <ael-moua@e2r8p2.1337.ma>
2025-03-09 23:37:16 +0000

    need ex00

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-moua <ael-moua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 21:48:22 by ael-moua          #+#    #+#             */
-/*   Updated: 2025/03/08 21:58:02 by ael-moua         ###   ########.fr       */
+/*   Updated: 2025/03/09 23:36:53 by ael-moua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,38 @@ std::string Phonebook::truncate(const std::string str) {
         return str;
 };
 
+int stoi(std::string num, int index)
+{
+    int i = 0;
+    int result = 0;
+    int cond = -1;
+    
+    while  (num[i]  && isspace(num[i]))
+        i++;
+    if (num[i] =='-')
+        return (-1);
+    else if (num[i] =='+')
+        i++;
+    while (num[i] && isdigit(num[i]))
+    {
+        cond = 1;
+        result = (result * 10 ) + (num[i] - '0');
+        if(result > 8 || result > index -1)
+            return (-1);
+        i++;
+    }
+    while  (num[i]  && isspace(num[i]))
+            i++;
+    if (num[i] || cond != 1)
+        return(-1);
+    return (result);
+}
 void Phonebook::print()
 {
     int i ;
     int index = 0;
 
+    std::string field;
     system("clear");
     std::cout << std::setw(10)<< "Index"
               << "|" << std::setw(10) << "First Name"
@@ -52,14 +79,16 @@ void Phonebook::print()
                   << "|" << std::setw(10) << truncate(phonebook[i].get_nickname()) << std::endl;
 
     }
+    while (1)
+    {
         std::cout << "Enter the index of the contact to display :";
-        std::cin >> index;
-        
-        if (index >= 0 && index < i)
-        {
-            phonebook[index].print();
+        std::getline(std::cin, field);
+        if (!field.compare("exit"))
             break;
-        } else 
+        index = stoi(field, i);
+        if (index > (-1))
+            phonebook[index].print();
+        else
             std::cout << "Invalid index." << std::endl;
-
+    }
 }
